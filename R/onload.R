36835eb5f597d98cf7e96a1fce7cd04a4ff7e7bc
Scott Chamberlain <myrmecocystus+r@gmail.com>
2018-11-05 23:10:03 +0000

    version 0.2.9

--- R/onload.R
+++ R/onload.R
@@ -1,40 +1,12 @@
 http_lib_adapter_registry <- NULL
-
 .onLoad <- function(libname, pkgname) {
   # set defaults for webmockr
   webmockr_configure()
 
-  # assign crul adapter for now by default
-  ##  because it's the only http lib supported for now
-  ##  later change to making user set the adapter themselves
+  # assign crul and httr adapters
+  # which doesn't require those packages loaded yet
   x <- HttpLibAdapaterRegistry$new()
   x$register(CrulAdapter$new())
   x$register(HttrAdapter$new())
   http_lib_adapter_registry <<- x
-
-  # initialize empty stub registry on package load
-  # webmockr_stub_registry <<- new.env()
-  # webmockr_stub_registry <- webmockr::StubRegistry$new()
 }
-
-# .onAttach <- function(libname, pkgname) {
-#   #base::unlockBinding("request_perform", as.environment("package:httr"))
-#   utils::assignInNamespace("request_perform", request_perform, "httr")
-#   #base::lockBinding("request_perform", as.environment("package:httr"))
-# }
-
-# .onAttach <- function(libname, pkgname) {
-#   when_attached("httr", {
-#     utils::assignInNamespace("request_perform", request_perform, "httr")
-#   })
-# }
-#
-# when_attached <- function(pkg, action) {
-#   if (is_attached(pkg)) {
-#     action
-#   } else {
-#     setHook(packageEvent(pkg, "attach"), function(...) action)
-#   }
-# }
-#
-# is_attached <- function(pkg) paste0("package:", pkg) %in% search()
