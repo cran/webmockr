cf60b48d3b61360a73ec544f7e216ee18ba01705
Scott Chamberlain <myrmecocystus+r@gmail.com>
2024-07-23 05:50:02 +0000

    version 1.0.0

--- tests/testthat/test-uri_regex.R
+++ tests/testthat/test-uri_regex.R
@@ -1,15 +1,16 @@
 context("uri_regex")
 
 test_that("uri_regex with crul", {
-  stub_request("get", uri_regex = "httpbin.org/.+") %>%
+  stub_request("get", uri_regex = "hb.opencpu.org/.+") %>%
     to_return(body = list(foo = "bar"))
 
   library(crul)
   enable(adapter = "crul")
+  webmockr_disable_net_connect()
   
   invisible(
     lapply(c('elephants', 'bears', 'leaves', 'foo', 'bar'), function(z) {
-      expect_true(HttpClient$new("https://httpbin.org")$get(z)$success())
+      expect_true(HttpClient$new(hb())$get(z)$success())
     })
   )
 
@@ -41,14 +42,14 @@ test_that("uri_regex with crul", {
 stub_registry_clear()
 
 test_that("uri_regex with httr", {
-  stub_request("get", uri_regex = "httpbin.org/.+") %>%
+  stub_request("get", uri_regex = "hb.opencpu.org/.+") %>%
     to_return(body = list(foo = "bar"))
 
   library(httr)
   enable(adapter = "httr")
   invisible(
     lapply(c('elephants', 'bears', 'leaves', 'foo', 'bar'), function(z) {
-      expect_false(http_error(GET(file.path("https://httpbin.org", z))))
+      expect_false(http_error(GET(file.path(hb(), z))))
     })
   )
 
@@ -72,6 +73,53 @@ test_that("uri_regex with httr", {
       url <- sprintf("https://%s.io", z)
       res <- GET(url, path = z)
       expect_is(res, "response")
+      expect_true(grepl(res$url, file.path(url, z), ignore.case = TRUE))
+    })
+  )
+})
+
+stub_registry_clear()
+
+
+test_that("uri_regex with httr2", {
+  skip_if_not_installed("httr2")
+  
+  stub_request("get", uri_regex = "hb.opencpu.org/.+") %>%
+    to_return(body = list(foo = "bar"))
+
+  library(httr2)
+  enable(adapter = "httr2")
+  invisible(
+    lapply(c('elephants', 'bears', 'leaves', 'foo', 'bar'), function(z) {
+      req <- request(file.path(hb(), z))
+      expect_false(resp_is_error(req_perform(req)))
+    })
+  )
+
+  # more complicated regex
+  stub_request("get", uri_regex = "[Aa].+\\.io/apple/")
+  invisible(
+    lapply(c('Anounce', 'apple', 'Afar', 'after'), function(z) {
+      req <- request(sprintf("https://%s.io/apple", z))
+      expect_false(resp_is_error(req_perform(req)))
+      req2 <- request(sprintf("https://%s.io/fruit", z))
+      expect_error(req_perform(req2),
+        "Real HTTP connections are disabled")
+    })
+  )
+
+  # regex to match any URL
+  ## https://github.com/ropensci/webmockr/issues/113 
+  ## when matching any url with `.+`, it would lead to an empty url in response
+  ##  object, at least with crul
+  stub_request("get", uri_regex = ".+")
+  invisible(
+    lapply(c('Anounce', 'apple', 'Afar', 'after'), function(z) {
+      url <- sprintf("https://%s.io", z)
+      # res <- GET(url, path = z)
+      req <- request(url) %>% req_url_path_append(z)
+      res <- req_perform(req)
+      expect_is(res, "httr2_response")
       expect_true(grepl(res$url, file.path(url, z), ignore.case = TRUE))
     })
   )
